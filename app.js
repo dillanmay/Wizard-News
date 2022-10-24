0484f4bd54da3f46bf12c7593c0adcb6ad6fde0b
AmoghAmar <aamogh11@gmail.com>
2022-10-24 14:13:46 -0500

    Amogh: Server Setup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
-
+app.use(morgan('dev'))
 app.get("/", (req, res) => res.send("Hello World!"));
 
 const PORT = 1337;
