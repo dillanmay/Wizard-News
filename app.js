d50446fad814b5aece399f593b0cdf42611d7e33
AmoghAmar <aamogh11@gmail.com>
2022-10-25 12:10:50 -0500

    Amogh: Posts

--- app.js
+++ app.js
@@ -8,6 +8,32 @@ app.use(express.static('public'));
 
 app.use(morgan("dev"));
 
+app.get('/posts/:id', (req, res) => {
+  const id = req.params.id;
+  const post = postBank.find(id);
+  res.send(`<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>
+    </div>
+  </body>
+</html>`);
+});
+
 app.get("/", (req, res) => {
   const posts = postBank.list();
 
