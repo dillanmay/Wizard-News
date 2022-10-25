44a0e113d3849bf02f99743e6ca7da23ce495daf
Dillan May <dillanmay_6@yahoo.com>
2022-10-25 10:27:28 -0500

    added styling to the database

--- app.js
+++ app.js
@@ -3,36 +3,43 @@ const morgan = require("morgan");
 const postBank = require("./postBank");
 const app = express();
 
-app.use(morgan('dev'));
+app.use(express.static('public'));
 
 
-// app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => {
+  const posts = postBank.list();
 
-const posts = postBank.list();
-
-const html = `<!DOCTYPE html>
-<html>
-<head>
-  <title>Wizard News</title>
-</head>
-<body>
-  <ul>
-    ${posts.map(post => `<li>${post.title}</li>`)}
-  </ul>
-</body>
+  const html = `<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Wizard News</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>
+  <body>
+    <div class="news-list">
+      <header><img src="/logo.png"/>Wizard News</header>
+      ${posts.map(post => `
+        <div class='news-item'>
+          <p>
+            <span class="news-position">${post.id}. ▲</span>${post.title}
+            <small>(by ${post.name})</small>
+          </p>
+          <small class="news-info">
+            ${post.upvotes} upvotes | ${post.date}
+          </small>
+        </div>`
+      ).join('')}
+    </div>
+  </body>
 </html>`;
 
-res.send(html);
+  res.send(html);
 });
-
-
-
-
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
-
